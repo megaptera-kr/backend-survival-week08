75b981cf513a0bfc5a31e1727a6e364f87ea7203
Ohjinn <hjin9445@gmail.com>
2023-06-05 20:22:38 +0900

    model_complete

---
+++ src/main/java/kr/megaptera/assignment/models/EntityId.java
@@ -0,0 +1,41 @@
+package kr.megaptera.assignment.models;
+
+import com.github.f4b6a3.tsid.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class EntityId {
+    @Column(name = "id")
+    private String value;
+
+    protected EntityId() {}
+
+    protected EntityId(String value) {
+        this.value = value;
+    }
+
+    protected static String newTsid() {
+        return newTsid().toString();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EntityId entityId = (EntityId) o;
+        return Objects.equals(value, entityId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
