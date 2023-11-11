1a0d5cd812838f6637335bb1802c4ccc04b25dfe
hoeseong <hoeseong19@naver.com>
2023-11-11 23:05:50 +0900

    feat(product): ProductRepository findAll return type 재정의

--- src/main/java/kr/megaptera/assignment/repositories/ProductRepository.java
+++ src/main/java/kr/megaptera/assignment/repositories/ProductRepository.java
@@ -3,7 +3,9 @@ package kr.megaptera.assignment.repositories;
 import kr.megaptera.assignment.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends CrudRepository<Product, UUID> {
+    List<Product> findAll();
 }
