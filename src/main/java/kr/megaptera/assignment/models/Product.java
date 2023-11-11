cf43df4177ecc849b28653c362d764ea682e033f
hoeseong <hoeseong19@naver.com>
2023-11-11 22:45:49 +0900

    feat(product): Product 생성

---
+++ src/main/java/kr/megaptera/assignment/models/Product.java
@@ -0,0 +1,25 @@
+package kr.megaptera.assignment.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Product {
+    @Id
+    UUID id;
+    @Column
+    String name;
+    @Column
+    Long price;
+
+    private Product() {
+    }
+
+    public Product(String name, Long price) {
+        this.name = name;
+        this.price = price;
+    }
+}
