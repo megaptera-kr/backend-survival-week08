fa3e824b24be8084ce6300fbbc994f73ffdbecd7
hoeseong <hoeseong19@naver.com>
2023-11-11 22:45:58 +0900

    feat(product): ProductRepository 생성

---
+++ src/main/java/kr/megaptera/assignment/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package kr.megaptera.assignment.repositories;
+
+import kr.megaptera.assignment.models.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends CrudRepository<Product, UUID> {
+}
