3dd7c27d24a78e1225fed04ab43163966d093046
hoeseong <hoeseong19@naver.com>
2023-11-12 17:10:14 +0900

    fix(product): Product entity id 자동 생성되도록 수정

--- src/main/java/kr/megaptera/assignment/models/Product.java
+++ src/main/java/kr/megaptera/assignment/models/Product.java
@@ -1,14 +1,13 @@
 package kr.megaptera.assignment.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     UUID id;
     @Column
     String name;
